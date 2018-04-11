a453c52737216e0462b368bd62fc1366af00ac45
andreaskari <aaskari96@gmail.com>
2018-04-10 19:19:31 -0700

    revert

--- app.js
+++
@@ -1,9 +0,0 @@
-// app.js
-var express = require('express');
-var app = express();
-var db = require('./db.js');
-
-var UserController = require('./user/UserController.js');
-app.use('/user', UserController); 
-
-module.exports = app;
--- db.js
+++
@@ -1,2 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://snejad123:Shayaun1994@ds235239.mlab.com:35239/mandril'); 
